380e819945b74fd68f5e9ea4bbd8316d3d817e55
rwbykz <dreamervt8@gmail.com>
2020-01-25 12:36:36 +0600

    App

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# crypto_calculator
-Crypto calculator with coinmarketcap API
+# README
+
+>Ruby on rails + React + Postgresql + Coinmarketcap's API app.
+>Simple app for calculate ur coins in $ in real-time.
+
+* Ruby version: 2.6.3p62
+
+* Rails version: 6.0.2.1
+
+* PostgreSQL version: 12.1
+
+* Node version: 10.18.1
+
+* NPM version: 6.13.4
+
+
---
+++ app/javascript/components/Search.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+  const searchResults = this.props.searchResults.map(curr => <li key={curr.id} data-id={curr.id} onClick={this.props.handleSelect} className="currency-list-item"><a href="#" className="currency"><span>{curr.name}</span> <span>{curr.currency_symbol}</span></a></li>)
+    return(
+      <div>
+        <h1>Cryptocurrency Portfolio Calculator</h1>
+        <form>
+          <div className="from-group">
+            <label>Search for a Currency:</label><br/>
+            <input onChange={this.props.handleChange} autoComplete="off" type="text" name="name" placeholder="Ex: Bitcoin, Litecoin, Ethereum, etc ..." value={this.props.name} className="field" />
+          </div>
+          <div className="currency-list">
+            {searchResults}
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search
